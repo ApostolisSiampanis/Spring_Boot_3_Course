94ebeb22a87be78a2bdf103ce60390bf2bbd6d82
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-11-28 21:59:30 +0200

    Part2, construction injection project created

---
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/example/springcoreddemo/SpringcoreddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcoreddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreddemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcoreddemoApplication.class, args);
+    }
+
+}
