1abf216590f489dfbc7f2686ac46a18564932875
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-11-30 21:23:58 +0200

    Primary annotation

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/example/springcoreddemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.example.springcoreddemo.rest;
+
+import com.example.springcoreddemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    @Autowired
+    public void DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
