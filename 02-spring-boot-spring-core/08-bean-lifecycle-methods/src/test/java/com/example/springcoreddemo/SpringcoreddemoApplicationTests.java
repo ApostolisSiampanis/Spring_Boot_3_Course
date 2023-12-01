373b3cf5e74c1f12752f618f8071f2393deafa11
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-12-02 00:51:03 +0200

    bean lifecycle, create and destroy

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/test/java/com/example/springcoreddemo/SpringcoreddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcoreddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreddemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
