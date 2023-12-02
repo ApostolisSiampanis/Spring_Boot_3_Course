0b0430f2b1d308664af244318bc8c1b48916a1eb
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-12-03 00:10:42 +0200

    student dao, first saved student

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.cruddemo;
 
+import com.example.cruddemo.dao.StudentDAO;
+import com.example.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,24 @@ public class CruddemoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
         return runner -> {
-            System.out.println("Hello World!");
+            createStudent(studentDAO);
         };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+
+        // create a student object
+        System.out.println("Creating new student object...");
+        Student tempSTudent = new Student("Paul", "Doe", "paul@gmail.com");
+
+        // save the student object
+        System.out.println("Saving the student...");
+        studentDAO.save(tempSTudent);
+
+        // display id of the saved object
+        System.out.println("Saved student. Generated id: " + tempSTudent.getId());
     }
 }
