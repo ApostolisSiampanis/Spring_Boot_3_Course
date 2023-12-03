fc8042423aabde36e1aeedf1871876faa3bf7816
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-12-03 21:39:42 +0200

    findAll students method

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -22,8 +24,21 @@ public class CruddemoApplication {
 
             // createMultipleStudents(studentDAO);
 
-            readStudent(studentDAO);
+            // readStudent(studentDAO);
+
+            queryForStudents(studentDAO);
         };
+    }
+
+    private void queryForStudents(StudentDAO studentDAO) {
+
+        // get a list of students
+        List<Student> theStudents = studentDAO.findAll();
+        
+        // display list of students
+        for (Student tempStudent : theStudents) {
+            System.out.println(tempStudent);
+        }
     }
 
     private void readStudent(StudentDAO studentDAO) {
--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/main/java/com/example/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/main/java/com/example/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.example.cruddemo.dao;
 
 import com.example.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO{
@@ -28,5 +31,14 @@ public class StudentDAOImpl implements StudentDAO{
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        // return query results
+        return theQuery.getResultList();
     }
 }
