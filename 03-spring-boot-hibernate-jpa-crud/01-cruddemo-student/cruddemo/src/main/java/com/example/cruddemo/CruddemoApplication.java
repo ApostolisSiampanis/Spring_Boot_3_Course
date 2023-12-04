e8d5766409eef7adcd9a3afd977778c4e2a387a8
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-12-04 23:48:14 +0200

    update method

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -28,8 +28,28 @@ public class CruddemoApplication {
 
             // queryForStudents(studentDAO);
 
-            queryForStudentsByLastName(studentDAO);
+            // queryForStudentsByLastName(studentDAO);
+
+            updateStudent(studentDAO);
         };
+    }
+
+    private void updateStudent(StudentDAO studentDAO) {
+
+        // retrieve student based on the id: primary key
+        int studentId = 1;
+        System.out.println("Getting student with id: " + studentId);
+        Student myStudent = studentDAO.findById(studentId);
+
+        // change first name to "John"
+        System.out.println("Updating student...");
+        myStudent.setFirstName("John");
+
+        // update the student
+        studentDAO.update(myStudent);
+
+        // display the updated student
+        System.out.println("Updated student: " + myStudent);
     }
 
     private void queryForStudentsByLastName(StudentDAO studentDAO) {
--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/main/java/com/example/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/cruddemo/src/main/java/com/example/cruddemo/dao/StudentDAOImpl.java
@@ -53,4 +53,10 @@ public class StudentDAOImpl implements StudentDAO{
         //return query results
         return theQuery.getResultList();
     }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+        entityManager.merge(theStudent);
+    }
 }
