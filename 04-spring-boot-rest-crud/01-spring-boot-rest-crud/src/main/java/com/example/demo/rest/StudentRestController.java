e788ad7746a1739a960ed2f4930999d3eede5256
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-12-05 23:40:41 +0200

    create PostConstruct method for the data

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.rest;
 
 import com.example.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +13,24 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return list of students
+    private List<Student> theStudents;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    // define @PostConstruct to load the student data... only once!
 
-        List<Student> theStudents = new ArrayList<>();
+    @PostConstruct
+    public void  loadData() {
+
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
+    }
+
+    // define endpoint for "/students" - return list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return theStudents;
     }
