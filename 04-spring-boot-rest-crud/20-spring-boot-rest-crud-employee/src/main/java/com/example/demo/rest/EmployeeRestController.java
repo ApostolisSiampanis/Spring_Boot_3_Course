e004cd7e5ce3baea2e2b72e11a5ba30c327e9201
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-12-12 13:56:04 +0200

    find a single student

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/demo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/demo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.Employee;
 import com.example.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +28,17 @@ public class EmployeeRestController {
         return employeeService.findAll();
     }
 
+    // add mapping for GET /employees/{employeeId}
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
+    }
 }
--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -2,8 +2,10 @@ package com.example.demo.service;
 
 import com.example.demo.dao.EmployeeDAO;
 import com.example.demo.entity.Employee;
+import com.example.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,12 +15,29 @@ public class EmployeeServiceImpl implements EmployeeService{
     private EmployeeDAO employeeDAO;
 
     @Autowired
-    private EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
         employeeDAO = theEmployeeDAO;
     }
 
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
     }
 }
