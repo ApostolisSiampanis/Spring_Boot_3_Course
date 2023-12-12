910a3b370e29318b04aee767c4abea999936a231
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-12-12 17:30:43 +0200

    configuration, pagination and sorting

--- 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/example/demo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/40-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/example/demo/dao/EmployeeRepository.java
@@ -3,5 +3,6 @@ package com.example.demo.dao;
 import com.example.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
