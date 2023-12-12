4e95564972fe587807d22ea1f1cfffd8f5192e29
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-12-12 14:25:11 +0200

    delete an employee

--- 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/demo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/20-spring-boot-rest-crud-employee/src/main/java/com/example/demo/rest/EmployeeRestController.java
@@ -64,4 +64,22 @@ public class EmployeeRestController {
         return dbEmployee;
     }
 
+    // add mapping for DELETE /employees/{employeeId} - delete employee
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
+    
 }
