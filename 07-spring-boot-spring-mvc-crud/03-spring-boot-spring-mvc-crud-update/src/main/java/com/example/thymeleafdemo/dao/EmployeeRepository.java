5d67e76b115e01c7ce9569235df25e2d65a9c67a
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-12-22 17:58:50 +0200

    update employees

---
+++ 07-spring-boot-spring-mvc-crud/03-spring-boot-spring-mvc-crud-update/src/main/java/com/example/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.example.thymeleafdemo.dao;
+
+import com.example.thymeleafdemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
+}
