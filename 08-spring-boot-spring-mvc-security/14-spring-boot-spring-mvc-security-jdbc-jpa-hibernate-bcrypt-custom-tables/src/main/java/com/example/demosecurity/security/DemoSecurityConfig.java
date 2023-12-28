29b7c07636427534739d6a8bda20ad420fb81d23
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-12-29 01:16:24 +0200

    bcrypt custom tables

---
+++ 08-spring-boot-spring-mvc-security/14-spring-boot-spring-mvc-security-jdbc-jpa-hibernate-bcrypt-custom-tables/src/main/java/com/example/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,77 @@
+package com.example.demosecurity.security;
+
+import com.example.demosecurity.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(UserService userService) {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService); // set the custom user details service
+        auth.setPasswordEncoder(passwordEncoder()); // set the password encoder - bcrypt
+        return auth;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                    configurer
+                            .requestMatchers("/").hasRole("EMPLOYEE")
+                            .requestMatchers("/leaders/**").hasRole("MANAGER")
+                            .requestMatchers("/systems/**").hasRole("ADMIN")
+                            .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                )
+                .logout(logout -> logout.permitAll()
+                )
+                .exceptionHandling(configurer ->
+                        configurer.accessDeniedPage("/access-denied")
+                );
+
+        return http.build();
+    }
+
+    /*
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+    */
+}
