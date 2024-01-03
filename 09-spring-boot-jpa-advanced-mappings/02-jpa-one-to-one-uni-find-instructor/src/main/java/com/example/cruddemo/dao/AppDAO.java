a16dd04dc79c9e06ffb27378c3449e74a5f6163e
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2024-01-03 20:24:10 +0200

    find an instructor

---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-uni-find-instructor/src/main/java/com/example/cruddemo/dao/AppDAO.java
@@ -0,0 +1,10 @@
+package com.example.cruddemo.dao;
+
+import com.example.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+}
