ff663e5050f18b447f99cbfca1a856ce3c927123
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2024-01-03 20:37:13 +0200

    delete an instructor

---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-one-uni-delete-instructor/src/main/java/com/example/cruddemo/dao/AppDAO.java
@@ -0,0 +1,12 @@
+package com.example.cruddemo.dao;
+
+import com.example.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+}
