378bff800d34db43e6d1e781ec9ec6c5b5747bca
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2024-01-07 11:13:17 +0200

    pointcut declarations

---
+++ 10-spring-boot-aop/10-spring-boot-aop-pointcut-declarations/src/main/java/com/example/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,10 @@
+package com.example.aopdemo.dao;
+
+import com.example.aopdemo.Account;
+
+public interface AccountDAO {
+
+    void addAccount(Account theAccount, boolean vipFlag);
+
+    boolean doWork();
+}
