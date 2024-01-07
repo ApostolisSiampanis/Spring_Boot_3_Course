babf0bae5952c5ea6330e80f425f82cc558f6f9c
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2024-01-07 22:51:43 +0200

    around advice rethrow exception

---
+++ 10-spring-boot-aop/21-spring-boot-aop-demo-around-advice-handle-exception/src/main/java/com/example/aopdemo/service/TrafficFortuneServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.aopdemo.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class TrafficFortuneServiceImpl implements TrafficFortuneService {
+
+    @Override
+    public String getFortune() {
+
+        // simulate a delay
+
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // return a fortune
+        return "Expect heavy traffic this morning";
+    }
+
+    @Override
+    public String getFortune(boolean tripWire) {
+
+
+        if (tripWire) {
+            throw new RuntimeException("Major accident! Highway is closed!");
+        }
+
+        return getFortune();
+    }
+}
