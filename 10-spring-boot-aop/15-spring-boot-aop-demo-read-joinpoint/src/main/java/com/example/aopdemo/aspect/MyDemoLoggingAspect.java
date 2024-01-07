98c34b530a1432e1cd0b230d06da12c0c4d6d61c
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2024-01-07 18:18:45 +0200

    read joinpoint

---
+++ 10-spring-boot-aop/15-spring-boot-aop-demo-read-joinpoint/src/main/java/com/example/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,46 @@
+package com.example.aopdemo.aspect;
+
+import com.example.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+    @Before("com.example.aopdemo.aspect.AopExpressionsUtil.forDaoPackageNoGetterSetter()")
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+        System.out.println("\n=====>>> Executing @Before advice on method");
+
+        // display the method signature
+        MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+
+        System.out.println("Method: " + methodSignature);
+
+        // display method arguments
+
+        // get args
+        Object[] args = theJoinPoint.getArgs();
+
+        // loop through args
+        for (Object tempArg : args) {
+            System.out.println(tempArg);
+
+            if (tempArg instanceof Account) {
+
+                // downcast and print Account specific stuff
+                Account theAccount = (Account) tempArg;
+
+                System.out.println("account name: " + theAccount.getName());
+                System.out.println("account level: " + theAccount.getLevel());
+            }
+        }
+
+    }
+
+}
