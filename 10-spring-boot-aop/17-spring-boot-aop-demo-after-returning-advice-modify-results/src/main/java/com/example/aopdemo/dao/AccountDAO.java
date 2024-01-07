ea2174d6568f4bf2d32152116ea09f495de740ba
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2024-01-07 20:12:32 +0200

    after returning advice modify results

---
+++ 10-spring-boot-aop/17-spring-boot-aop-demo-after-returning-advice-modify-results/src/main/java/com/example/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,24 @@
+package com.example.aopdemo.dao;
+
+import com.example.aopdemo.Account;
+
+import java.util.List;
+
+public interface AccountDAO {
+
+    // add a new method: findAccounts()
+
+    List<Account> findAccounts();
+
+    void addAccount(Account theAccount, boolean vipFlag);
+
+    boolean doWork();
+
+    public String getName();
+
+    public void setName(String name);
+
+    public String getServiceCode();
+
+    public void setServiceCode(String serviceCode);
+}
