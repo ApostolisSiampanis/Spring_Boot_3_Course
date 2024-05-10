ceb89c723e8b0240520db331ad720a3c4d9eb32a
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-05-10 14:06:28 +0300

    small fix on put method

--- 05-spring-boot-rest-security/01-spring-boot-rest-security-employee-jdbc-bcrypt/src/main/java/com/example/demo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/01-spring-boot-rest-security-employee-jdbc-bcrypt/src/main/java/com/example/demo/security/DemoSecurityConfig.java
@@ -31,7 +31,7 @@ public class DemoSecurityConfig {
                     .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
                     .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
                     .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                    .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("MANAGER")
                     .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
         );
 
